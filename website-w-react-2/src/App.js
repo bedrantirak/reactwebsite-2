6d085756ca2d9ecf69f4c37e15958f615b784d01
bedrantirak <bdrntrk@gmail.com>
2020-10-17 14:01:10 +0300

    Update App.js

--- website-w-react-2/src/App.js
+++ website-w-react-2/src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
-
+import Navbar from "./components/Navbar"
 import './App.css';
+import {BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route path="/" />
+      </Switch>
+    </Router>
   );
 }
 
