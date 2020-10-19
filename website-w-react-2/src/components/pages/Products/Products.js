a9fe7e082ca32c14b95fc4a48fcdb51deda49a49
bedrantirak <bdrntrk@gmail.com>
2020-10-19 11:21:39 +0300

    Update Products.js

---
+++ website-w-react-2/src/components/pages/Products/Products.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import HeroSection from '../../HeroSection';
+import { homeObjTwo} from './Data';
+import Pricing from '../../Pricing';
+
+function Products() {
+  return (
+    <>
+     
+      <Pricing />
+      <HeroSection {...homeObjTwo} />
+    </>
+  );
+}
+
+export default Products;
