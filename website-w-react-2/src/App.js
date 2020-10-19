c5d0e14b1ed62b3bd19071f3cf16bd751e2aca7d
bedrantirak <bdrntrk@gmail.com>
2020-10-19 11:21:31 +0300

    Update App.js

--- website-w-react-2/src/App.js
+++ website-w-react-2/src/App.js
@@ -5,6 +5,7 @@ import {BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './components/pages/HomePage/Home';
 import Services from './components/pages/Services/Services';
 import Products from './components/pages/Products/Products';
+import SignUp from './components/pages/SignUp/SignUp';
 import Footer from './components/pages/Footer/Footer';
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
         <Route path="/"  exact component={Home}/>
         <Route path="/services" component={Services}/>
         <Route path="/products" component={Products}/>
+        <Route path="/sign-up" component={signUp}/>
 
       </Switch>
       <Footer></Footer>
